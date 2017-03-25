e4b7fcc54679c8b8545431d72edd27b3bc5da867
Fahim Chowdhury <fahim.chowdhury1985@gmail.com>
2017-03-25 01:45:56 +0000

    added bind polyfill

--- scraper.js
+++ scraper.js
@@ -188,9 +188,32 @@ function writeData(data) {
 
 }
 */
-function screenshot(tableData) {
-    console.log(JSON.stringify(tableData, null, 2));
-    writeData(tableData);
+if (!Function.prototype.bind) {
+  Function.prototype.bind = function(oThis) {
+    if (typeof this !== 'function') {
+      // closest thing possible to the ECMAScript 5
+      // internal IsCallable function
+      throw new TypeError('Function.prototype.bind - what is trying to be bound is not callable');
+    }
+
+    var aArgs   = Array.prototype.slice.call(arguments, 1),
+        fToBind = this,
+        fNOP    = function() {},
+        fBound  = function() {
+          return fToBind.apply(this instanceof fNOP
+                 ? this
+                 : oThis,
+                 aArgs.concat(Array.prototype.slice.call(arguments)));
+        };
+
+    if (this.prototype) {
+      // Function.prototype doesn't have a prototype property
+      fNOP.prototype = this.prototype; 
+    }
+    fBound.prototype = new fNOP();
+
+    return fBound;
+  };
 }
 
 
